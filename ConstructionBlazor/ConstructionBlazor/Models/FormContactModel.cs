49a1ecd9c7ed9f1de85fcc59d08f158327e6f51a
PhiHung <PhiHung@DESKTOP-71ANV7F>
2024-08-29 21:23:24 +0700

    Add project files.

---
+++ ConstructionBlazor/ConstructionBlazor/Models/FormContactModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConstructionBlazor.Models
+{
+    public class FormContactModel
+    {
+        [Required(ErrorMessage = "Tên bị trống")]
+        public string Name { get; set; } 
+        [Required(ErrorMessage = "Email bị trống")]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Phone(ErrorMessage = "Số điện thoại không hợp lệ")]
+        [Required(ErrorMessage = "Số điện thoại bị trống")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Thông tin chi tiết bị trống")]
+        public string Description {  get; set; } 
+    }
+}
